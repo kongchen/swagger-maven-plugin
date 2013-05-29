012abe16b983a6438a1c100b994a7282ed7a94c0
kongchen <kongchen@gmail.com>
2013-05-29 22:15:15 +0800

    1) Add support to generate json files for swagger-ui; 2) To simplify the configuration, do not support multi apiSource anymore. 3) Simplify README.md

--- src/main/java/com/github/kongchen/swagger/docgen/AbstractDocumentSource.java
+++ src/main/java/com/github/kongchen/swagger/docgen/AbstractDocumentSource.java
@@ -1,7 +1,13 @@
 package com.github.kongchen.swagger.docgen;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wordnik.swagger.core.Documentation;
 
 /**
@@ -15,7 +21,13 @@ public abstract class AbstractDocumentSource {
 
     private String apiVersion;
 
-    public abstract List<Documentation> toSwaggerDocuments() throws Exception;
+    private ObjectMapper mapper = new ObjectMapper();
+
+    public abstract void documentsIn() throws Exception, GenerateException;
+
+    protected Documentation serviceDocument;
+
+    List<Documentation> validDocuments = new LinkedList<Documentation>();
 
     public String getBasePath() {
         return basePath;
@@ -31,5 +43,75 @@ public abstract class AbstractDocumentSource {
 
     public void setApiVersion(String apiVersion) {
         this.apiVersion = apiVersion;
+    }
+
+    protected void acceptDocument(Documentation doc) {
+        validDocuments.add(doc);
+    }
+
+    public List<Documentation> getValidDocuments() {
+        return validDocuments;
+    }
+
+    public void writeSwaggerDocuments(String outputDirectory) throws GenerateException {
+        if (outputDirectory == null) {
+            return;
+        }
+        File dir = new File(outputDirectory);
+        if (dir.isFile()) {
+            throw new GenerateException(
+                    String.format("Swagger-outputDirectory[%s] must be a directory!", outputDirectory));
+        }
+
+        if (!dir.exists()) {
+            try {
+                FileUtils.forceMkdir(dir);
+            } catch (IOException e) {
+                throw new GenerateException(
+                        String.format("Create Swagger-outputDirectory[%s] failed.", outputDirectory));
+            }
+        }
+        cleanupOlds(dir);
+
+        writeInDirectory(dir, serviceDocument);
+        for (Documentation doc : validDocuments) {
+            writeInDirectory(dir, doc);
+        }
+    }
+
+    private void cleanupOlds(File dir) {
+        if (dir.listFiles() != null) {
+            for (File f : dir.listFiles()) {
+                if (f.getName().endsWith("json")) {
+                    f.delete();
+                }
+            }
+        }
+    }
+
+    private String resourcePathToFilename(String resourcePath) {
+        if (resourcePath == null) {
+            return "service.json";
+        }
+        String name = resourcePath;
+        if (resourcePath.startsWith("/")) {
+            name = resourcePath.substring(1);
+        }
+        name = name.replaceAll("/", "_");
+
+        return name + ".json";
+    }
+
+    private void writeInDirectory(File dir, Documentation doc) throws GenerateException {
+        String filename = resourcePathToFilename(doc.getResourcePath());
+        File serviceFile = new File(dir, filename);
+        try {
+            while (!serviceFile.createNewFile()) {
+                serviceFile.delete();
+            }
+            mapper.writerWithDefaultPrettyPrinter().writeValue(serviceFile, doc);
+        } catch (IOException e) {
+            throw new GenerateException(e);
+        }
     }
 }
--- src/main/java/com/github/kongchen/swagger/docgen/remote/RemoteDocumentSource.java
+++ src/main/java/com/github/kongchen/swagger/docgen/remote/RemoteDocumentSource.java
@@ -3,11 +3,7 @@ package com.github.kongchen.swagger.docgen.remote;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.LinkedList;
-import java.util.List;
 
-import com.github.kongchen.swagger.docgen.AbstractDocumentSource;
-import com.github.kongchen.swagger.docgen.LogAdapter;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
@@ -16,6 +12,8 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.kongchen.swagger.docgen.AbstractDocumentSource;
+import com.github.kongchen.swagger.docgen.LogAdapter;
 import com.wordnik.swagger.core.Documentation;
 import com.wordnik.swagger.core.DocumentationEndPoint;
 
@@ -30,24 +28,27 @@ public class RemoteDocumentSource extends AbstractDocumentSource {
 
     private final URI requestURI;
 
+    ObjectMapper mapper = new ObjectMapper();
+
     public RemoteDocumentSource(URI requestURI) {
         LOG = new LogAdapter(LoggerFactory.getLogger(RemoteDocumentSource.class));
         this.requestURI = requestURI;
     }
 
-    public List<Documentation> toSwaggerDocuments() throws IOException {
+    public void documentsIn() throws IOException {
         HttpClient client = new DefaultHttpClient();
         HttpResponse response = client.execute(new HttpGet(requestURI));
-        ObjectMapper mapper = new ObjectMapper();
+
         Documentation doc = mapper.readValue(response.getEntity().getContent(), Documentation.class);
+        serviceDocument = doc;
 
         setApiVersion(doc.getApiVersion());
         setBasePath(doc.getBasePath());
         URIBuilder uriBuilder = new URIBuilder(requestURI);
         String path = uriBuilder.getPath();
 
-        List<Documentation> validDocuments = new LinkedList<Documentation>();
         for (DocumentationEndPoint endPoint : doc.getApis()) {
+
             String _endpoint = endPoint.getPath().replaceAll("/api-docs\\.\\{format\\}", "");
             uriBuilder.setPath((path + "/" + _endpoint).replaceAll("\\/\\/", "/"));
             String newURL = null;
@@ -60,9 +61,8 @@ public class RemoteDocumentSource extends AbstractDocumentSource {
             LOG.info("calling " + newURL);
             response = client.execute(new HttpGet(newURL));
             Documentation _doc = mapper.readValue(response.getEntity().getContent(), Documentation.class);
-            validDocuments.add(_doc);
+            acceptDocument(_doc);
         }
-
-        return validDocuments;
     }
+
 }
