388b7301450e27adaa648e63499dd69775703628
kongchen <kongchen@gmail.com>
2013-06-05 15:38:12 +0800

    to debug for travis build failed

--- src/test/java/com/github/kongchen/swagger/docgen/mavenplugin/ApiDocumentMojoTest.java
+++ src/test/java/com/github/kongchen/swagger/docgen/mavenplugin/ApiDocumentMojoTest.java
@@ -1,14 +1,18 @@
 package com.github.kongchen.swagger.docgen.mavenplugin;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -44,10 +48,27 @@ public class ApiDocumentMojoTest {
         while (true) {
             int expect = expectIs.read();
             int actual = testOutputIs.read();
+            if (expect != actual) {
+                System.out.println("generated file: ");
+                displayFileContent(new FileInputStream(new File("temp.html")));
+                System.out.println("base file: ");
+                displayFileContent(this.getClass().getResourceAsStream("/sample.html"));
+            }
             Assert.assertEquals(expect, actual);
             if (expect == -1) {
                 break;
             }
         }
+    }
+
+    private void displayFileContent(InputStream inputStream) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, Charset.forName("UTF-8")));
+        String line = reader.readLine();
+        while (line != null) {
+            System.out.print(line);
+            line = reader.readLine();
+        }
+        reader.close();
+
     }
 }
