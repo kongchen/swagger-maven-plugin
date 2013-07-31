f823c92d15e878d8201ee2aba5c00709ae20ad9e
kongchen <kongchen@gmail.com>
2013-07-31 21:30:55 +0800

    fix issue#17

---
+++ src/test/java/issue17/Child.java
@@ -0,0 +1,18 @@
+package issue17;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kongchen
+ * Date: 7/31/13
+ */
+public class Child extends Parent{
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
