255f0d48f278a2814901f6cfa6497e849bb4b4f5
kongchen <kongchen@gmail.com>
2014-01-20 13:10:44 +0800

    revert https://github.com/alfmateos/swagger-maven-plugin/commit/c699ae2a4710f71ee387e84f1670810d7e9abe31 unit test

--- src/test/java/com/github/kongchen/swagger/docgen/AbstractDocumentSourceTest.java
+++ src/test/java/com/github/kongchen/swagger/docgen/AbstractDocumentSourceTest.java
@@ -11,46 +11,43 @@ import org.testng.annotations.Test;
 
 public class AbstractDocumentSourceTest {
 
-  // Omit the '*.json' extension, so that swagger-ui can show it correctly.
+    public static final String OUTPUT_PATH = "foo/bar.json";
+    public static final String FLAT_OUTPUT_PATH = "foo_bar.json";
 
-  public static final String OUTPUT_PATH = "foo/bar";
-  public static final String FLAT_OUTPUT_PATH = "foo_bar";
+    @Test
+    public void testResourcePathToFilename() throws Exception {
+        AbstractDocumentSource abstractDocumentSource = getAbstractDocumentSource(true);
+        assertEquals(FLAT_OUTPUT_PATH, abstractDocumentSource.resourcePathToFilename("/foo/bar"));
+        assertEquals(FLAT_OUTPUT_PATH, abstractDocumentSource.resourcePathToFilename("/foo/bar/"));
+        assertEquals(FLAT_OUTPUT_PATH, abstractDocumentSource.resourcePathToFilename("foo/bar"));
+        assertEquals(FLAT_OUTPUT_PATH, abstractDocumentSource.resourcePathToFilename("foo/bar/"));
+        assertEquals("bar.json", abstractDocumentSource.resourcePathToFilename("bar"));
 
-  private AbstractDocumentSource getAbstractDocumentSource(final boolean useOutputFlatStructure) {
-    return new AbstractDocumentSource(new LogAdapter((Logger) null), null, null, null, null,
-        useOutputFlatStructure) {
-      @Override
-      public void loadDocuments() throws Exception, GenerateException {
+        abstractDocumentSource = getAbstractDocumentSource(false);
+        assertEquals(OUTPUT_PATH, abstractDocumentSource.resourcePathToFilename("/foo/bar"));
+        assertEquals(OUTPUT_PATH, abstractDocumentSource.resourcePathToFilename("/foo/bar/"));
+        assertEquals(OUTPUT_PATH, abstractDocumentSource.resourcePathToFilename("foo/bar"));
+        assertEquals(OUTPUT_PATH, abstractDocumentSource.resourcePathToFilename("foo/bar/"));
+        assertEquals("bar.json", abstractDocumentSource.resourcePathToFilename("bar"));
+    }
 
-      }
-    };
-  }
+    @Test
+    public void testCreateFile() throws Exception {
+        AbstractDocumentSource abstractDocumentSource = getAbstractDocumentSource(true);
 
-  @Test
-  public void testCreateFile() throws Exception {
-    AbstractDocumentSource abstractDocumentSource = getAbstractDocumentSource(true);
+        File target = abstractDocumentSource.createFile(new File("target"), OUTPUT_PATH);
+        assertTrue(target.getPath(), FilenameUtils.equalsNormalized("target/foo/bar.json", target.getPath()));
 
-    File target = abstractDocumentSource.createFile(new File("target"), OUTPUT_PATH);
-    assertTrue(target.getPath(), FilenameUtils.equalsNormalized("target/foo/bar", target.getPath()));
+        target = abstractDocumentSource.createFile(new File("target"), FLAT_OUTPUT_PATH);
+        assertTrue(target.getPath(), FilenameUtils.equalsNormalized("target/foo_bar.json", target.getPath()));
+    }
 
-    target = abstractDocumentSource.createFile(new File("target"), FLAT_OUTPUT_PATH);
-    assertTrue(target.getPath(), FilenameUtils.equalsNormalized("target/foo_bar", target.getPath()));
-  }
+    private AbstractDocumentSource getAbstractDocumentSource(final boolean useOutputFlatStructure) {
+        return new AbstractDocumentSource(new LogAdapter((Logger) null), null, null, null, null, useOutputFlatStructure) {
+            @Override
+            public void loadDocuments() throws Exception, GenerateException {
 
-  @Test
-  public void testResourcePathToFilename() throws Exception {
-    AbstractDocumentSource abstractDocumentSource = getAbstractDocumentSource(true);
-    assertEquals(FLAT_OUTPUT_PATH, abstractDocumentSource.resourcePathToFilename("/foo/bar"));
-    assertEquals(FLAT_OUTPUT_PATH, abstractDocumentSource.resourcePathToFilename("/foo/bar/"));
-    assertEquals(FLAT_OUTPUT_PATH, abstractDocumentSource.resourcePathToFilename("foo/bar"));
-    assertEquals(FLAT_OUTPUT_PATH, abstractDocumentSource.resourcePathToFilename("foo/bar/"));
-    assertEquals("bar", abstractDocumentSource.resourcePathToFilename("bar"));
-
-    abstractDocumentSource = getAbstractDocumentSource(false);
-    assertEquals(OUTPUT_PATH, abstractDocumentSource.resourcePathToFilename("/foo/bar"));
-    assertEquals(OUTPUT_PATH, abstractDocumentSource.resourcePathToFilename("/foo/bar/"));
-    assertEquals(OUTPUT_PATH, abstractDocumentSource.resourcePathToFilename("foo/bar"));
-    assertEquals(OUTPUT_PATH, abstractDocumentSource.resourcePathToFilename("foo/bar/"));
-    assertEquals("bar", abstractDocumentSource.resourcePathToFilename("bar"));
-  }
+            }
+        };
+    }
 }
