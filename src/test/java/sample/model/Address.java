4ea32152373d52d5b1ef53cc9cbab835cfa52d80
Kong Chen <kongchen@gmail.com>
2014-05-28 22:54:49 +0800

    Fix issue #64. sort properties according to `position` in @ApiModelProperty

--- src/test/java/sample/model/Address.java
+++ src/test/java/sample/model/Address.java
@@ -15,7 +15,7 @@ public class Address implements Entity<Integer> {
      * {@inheritDoc}
      */
     @XmlElement(name = "id")
-    @ApiModelProperty(value = "Address' indentifier")
+    @ApiModelProperty(value = "Address' indentifier", position = 2)
     @Override
     public Integer getId() {
         return id;
@@ -30,7 +30,7 @@ public class Address implements Entity<Integer> {
     }
 
     @XmlElement(name = "zip", required = false)
-    @ApiModelProperty(value = "The location")
+    @ApiModelProperty(value = "The zip code for this location", position = 3)
     public Long getZipCode() {
         return zipCode;
     }
@@ -40,7 +40,7 @@ public class Address implements Entity<Integer> {
     }
 
     @XmlElement(name = "location")
-    @ApiModelProperty(value = "The zip code for this location", required = false)
+    @ApiModelProperty(value = "The location", required = false, position = 1)
     public String getLocation() {
         return location;
     }
