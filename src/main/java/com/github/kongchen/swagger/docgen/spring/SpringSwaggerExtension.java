4f334c931dc7b593ba759dea7024d8b6afd1fc31
Andrew B <who@users.noreply.github.com>
2015-05-19 13:05:37 -0700

    Fixing an issue with RequestMapping string[]

--- src/main/java/com/github/kongchen/swagger/docgen/spring/SpringSwaggerExtension.java
+++ src/main/java/com/github/kongchen/swagger/docgen/spring/SpringSwaggerExtension.java
@@ -78,7 +78,7 @@ public class SpringSwaggerExtension extends AbstractSwaggerExtension implements 
                 qp.setProperty(schema);
             }
 
-            if (isArray || cls.isArray() || cls.isAssignableFrom(Collection.class)) {
+            if (isArray || Collection.class.isAssignableFrom(cls) || cls.isArray()) {
                 qp.setType("string");
             }
 
