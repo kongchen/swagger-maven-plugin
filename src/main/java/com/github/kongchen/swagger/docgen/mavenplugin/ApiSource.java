f5f55879ffa62503b0699c2fe25222a47f557890
dantran <dantran@gmail.com>
2015-12-02 12:17:41 -0800

    javadoc attachSwagger setting

--- src/main/java/com/github/kongchen/swagger/docgen/mavenplugin/ApiSource.java
+++ src/main/java/com/github/kongchen/swagger/docgen/mavenplugin/ApiSource.java
@@ -65,6 +65,11 @@ public class ApiSource {
     @Parameter
     private String swaggerDirectory;
 
+    /**
+     * <code>attachSwagger</code> triggers plugin execution to attach the generated
+     * wagger.json to Maven session for deployment purpose.  The attached classifier
+     * is the directory name of <code>swaggerDirectory</code>
+     */
     @Parameter
     private boolean attachSwagger;
 
