f8e8e6c7e1ffd6eaefb0f87284523ad4e2a18c87
rakk <Robert.Kapala@gmail.com>
2016-01-13 21:40:31 +0100

    Annotions util annotations, traversing its super methods/classes

--- src/main/java/com/github/kongchen/swagger/docgen/util/Annotations.java
+++ src/main/java/com/github/kongchen/swagger/docgen/util/Annotations.java
@@ -1,16 +1,38 @@
 package com.github.kongchen.swagger.docgen.util;
 
-import javax.ws.rs.GET;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 
 public class Annotations {
 
     public static <T extends Annotation> T get(Method method, Class<T> annotationClass) {
-        return method.getAnnotation(annotationClass);
+        T result = method.getAnnotation(annotationClass);
+        if (result == null) {
+            Class currentClass = method.getDeclaringClass();
+            if (currentClass != Object.class) {
+                Class superClass = currentClass.getSuperclass();
+                Method superClassMethod = null;
+                try {
+                    superClassMethod = superClass.getMethod(method.getName(), method.getParameterTypes());
+                } catch (NoSuchMethodException e) {
+
+                }
+                if (superClassMethod != null) {
+                    return get(superClassMethod, annotationClass);
+                }
+            }
+        }
+        return result;
     }
 
     public static <T extends Annotation> T get(Class<?> givenClass, Class<T> annotationClass) {
-        return givenClass.getAnnotation(annotationClass);
+        T result = givenClass.getAnnotation(annotationClass);
+        if (result == null) {
+            if (givenClass != Object.class) {
+                Class superClass = givenClass.getSuperclass();
+                return (T) get(superClass, annotationClass);
+            }
+        }
+        return result;
     }
 }
