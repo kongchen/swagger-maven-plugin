c456fd2acc1dc4f51e83dd718509a8bb2536ec9b
rakk <Robert.Kapala@gmail.com>
2016-01-15 08:33:29 +0100

    Additing unit tests for Annotations

---
+++ src/test/java/com/github/kongchen/swagger/docgen/util/AnnotationsTests.java
@@ -0,0 +1,47 @@
+package com.github.kongchen.swagger.docgen.util;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+
+public class AnnotationsTests {
+
+    public static <T extends Annotation> T get(Method method, Class<T> annotationClass) {
+        T result = method.getAnnotation(annotationClass);
+        if (result == null) {
+            Class superClass = getSuperClassIfNotObjectClass(method.getDeclaringClass());
+            if (superClass != null) {
+                Method superClassMethod = null;
+                try {
+                    superClassMethod = superClass.getMethod(method.getName(), method.getParameterTypes());
+                } catch (NoSuchMethodException e) {
+
+                }
+                if (superClassMethod != null) {
+                    return get(superClassMethod, annotationClass);
+                }
+            }
+        }
+        return result;
+    }
+
+    public static <T extends Annotation> T get(Class<?> givenClass, Class<T> annotationClass) {
+        T result = givenClass.getAnnotation(annotationClass);
+        if (result == null) {
+            Class superClass = getSuperClassIfNotObjectClass(givenClass);
+            if (superClass != null) {
+                return (T) get(superClass, annotationClass);
+            }
+        }
+        return result;
+    }
+
+    private static Class getSuperClassIfNotObjectClass(Class givenClass) {
+        if (givenClass != Object.class) {
+            Class superClass = givenClass.getSuperclass();
+            if (superClass != Object.class) {
+                return superClass;
+            }
+        }
+        return null;
+    }
+}
---
+++ src/test/java/com/github/kongchen/swagger/docgen/util/TestApiA.java
@@ -0,0 +1,7 @@
+package com.github.kongchen.swagger.docgen.util;
+
+/**
+ * Created by robertkapala on 15/01/16.
+ */
+public class TestApiA {
+}
