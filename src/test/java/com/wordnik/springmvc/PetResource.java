d72462b95e84147bd5f49aa60dd31937e725ef38
Konstantin Labun <konstantin.labun@gmail.com>
2017-04-27 07:32:36 +0300

    Added merged annotations support(see spring's @AliasFor annotation)

--- src/test/java/com/wordnik/springmvc/PetResource.java
+++ src/test/java/com/wordnik/springmvc/PetResource.java
@@ -36,13 +36,7 @@ import io.swagger.annotations.ExtensionProperty;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.Size;
 import java.util.ArrayList;
@@ -304,5 +298,13 @@ public class PetResource {
             produces = "application/json")
     public String testingVendorExtensions() {
         return null;
+    }
+
+    @ApiOperation("testingMergedAnnotations")
+    @GetMapping(
+            value = "/testingMergedAnnotations",
+            produces = "application/json")
+    public String testingMergedAnnotations() {
+        return "it works";
     }
 }
