259c061b00305073fc9a289d9f8c5b0e5de8d2f7
Yuliya Levanovich <yuliya.levanovich@connectis.nl>
2018-01-04 18:26:00 +0100

    restored original formatting

--- src/test/java/com/wordnik/jaxrs/MyResourceImpl.java
+++ src/test/java/com/wordnik/jaxrs/MyResourceImpl.java
@@ -20,15 +20,15 @@ import com.wordnik.sample.JavaRestResourceUtil;
 import com.wordnik.sample.data.PetData;
 import com.wordnik.sample.model.ListItem;
 import com.wordnik.sample.model.Pet;
+
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import java.util.ArrayList;
-import java.util.List;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
 
 @Path("/myResourceImpl")
 public class MyResourceImpl extends MyResourceAbstract<String> {
@@ -52,7 +52,7 @@ public class MyResourceImpl extends MyResourceAbstract<String> {
 
     //contrived example test case for swagger-maven-plugin issue #505
     /* (non-Javadoc)
-     * @see com.wordnik.jaxrs.MyResource#getListOfItems()
+	 * @see com.wordnik.jaxrs.MyResource#getListOfItems()
 	 */
     @Path("list")
     @Override
