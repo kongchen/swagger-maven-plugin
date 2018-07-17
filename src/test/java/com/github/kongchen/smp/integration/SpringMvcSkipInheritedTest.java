1f917ea47211b53a21a58050a7c09070c8494e82
Tomasz Juchniewicz <tjuchniewicz@gmail.com>
2018-07-17 16:58:46 +0200

    Introduce skipInheritingClasses parameter

---
+++ src/test/java/com/github/kongchen/smp/integration/SpringMvcSkipInheritedTest.java
@@ -0,0 +1,66 @@
+package com.github.kongchen.smp.integration;
+
+import static net.javacrumbs.jsonunit.JsonAssert.assertJsonEquals;
+import static net.javacrumbs.jsonunit.core.Option.IGNORING_ARRAY_ORDER;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.kongchen.swagger.docgen.mavenplugin.ApiDocumentMojo;
+
+import io.swagger.jaxrs.ext.SwaggerExtension;
+import io.swagger.jaxrs.ext.SwaggerExtensions;
+import net.javacrumbs.jsonunit.core.Configuration;
+
+public class SpringMvcSkipInheritedTest extends AbstractMojoTestCase {
+    private File swaggerOutputDir = new File(getBasedir(), "generated/swagger-ui-spring-skip-inherited");
+    private File docOutput = new File(getBasedir(), "generated/document-spring-skip-inherited.html");
+    private ApiDocumentMojo mojo;
+    private List<SwaggerExtension> extensions;
+
+    @Override
+	@BeforeMethod
+    protected void setUp() throws Exception {
+    	extensions = new ArrayList<SwaggerExtension>(SwaggerExtensions.getExtensions());
+    	super.setUp();
+
+        try {
+            FileUtils.deleteDirectory(swaggerOutputDir);
+            FileUtils.forceDelete(docOutput);
+        } catch (Exception e) {
+            //ignore
+        }
+
+        File testPom = new File(getBasedir(), "target/test-classes/plugin-config-springmvc-skip-inherited.xml");
+        mojo = (ApiDocumentMojo) lookupMojo("generate", testPom);
+    }
+
+    @Override
+    @AfterMethod
+    protected void tearDown() throws Exception {
+    	super.tearDown();
+    	SwaggerExtensions.setExtensions(extensions);
+    }
+   
+    @Test
+    public void testAssertGeneratedSwaggerSpecJson() throws MojoExecutionException, MojoFailureException, IOException {
+        mojo.execute();
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode actualJson = mapper.readTree(new File(swaggerOutputDir, "swagger.json"));
+        JsonNode expectJson = mapper.readTree(this.getClass().getResourceAsStream("/expectedOutput/swagger-spring-skip-inherited.json"));
+
+        assertJsonEquals(expectJson, actualJson, Configuration.empty().when(IGNORING_ARRAY_ORDER));
+    }
+}
